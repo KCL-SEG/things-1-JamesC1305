d7f1373388ccd4e8ec061c400a0c7513c1077007
James <james.curtis@kcl.ac.uk>
2022-10-09 14:36:31 +0100

    Changed format of Thing model

--- things/models.py
+++ things/models.py
@@ -3,6 +3,18 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False)
-    description = models.TextField(unique=False, blank=True, max_length=120)
-    quantity = models.PositiveSmallIntegerField(unique=False, validators=[MinValueValidator(0), MaxValueValidator(100)])
+    name = models.CharField(
+        max_length=30, 
+        unique=True, 
+        blank=False
+    )
+    description = models.TextField(
+        unique=False,
+        blank=True, 
+        max_length=120
+    )
+    quantity = models.PositiveSmallIntegerField(unique=False, 
+    validators=[
+        MinValueValidator(0), 
+        MaxValueValidator(100)
+        ])
