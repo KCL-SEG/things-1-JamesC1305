4d90ef4b4e4418e960c93c94e142a7e80a7020fe
James <james.curtis@kcl.ac.uk>
2022-10-09 13:51:36 +0100

    Add max_length parameter to CharField in Thing model.

--- things/models.py
+++ things/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=30)
     description = models.TextField()
     quantity = models.PositiveIntegerField()
