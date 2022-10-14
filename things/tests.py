c015a335d05cf59c94777914508f24bbd41b2ccb
James Curtis <james.curtis@kcl.ac.uk>
2022-10-14 15:44:31 +0100

    Added migrations for Thing

--- things/tests.py
+++ things/tests.py
@@ -1,4 +1,12 @@
 from django.test import TestCase
-from models import Thing
+from .models import Thing
 
 # Create your tests here.
+class ThingModelTestCase(TestCase):
+
+    def setUp(self):
+        self.thing = Thing.objects.create(
+            name = "Apple",
+            description = "Red and Shiny",
+            quantity = 32
+        )
